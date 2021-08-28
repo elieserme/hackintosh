32049e80f0b7b0f37d2816e04cd3b043366e34ef
Elieser <66266691+elieserme@users.noreply.github.com>
2021-08-28 00:07:08 -0300

    power button to sleep on scripts

--- scripts/power.sh
+++ scripts/power.sh
@@ -3,4 +3,5 @@ sudo pmset proximitywake 0
 sudo pmset -a hibernatemode 3
 sudo pmset -a standbydelaylow 7200
 sudo pmset -a standbydelayhigh 7200
+defaults write com.apple.loginwindow PowerButtonSleepsSystem -bool yes
 pmset -g live
