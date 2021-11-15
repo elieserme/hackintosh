0d2e0456e83603f097241e1fa30d077b3dd08bcb
Elieser <66266691+elieserme@users.noreply.github.com>
2021-11-14 22:02:45 -0300

    unique power script

--- scripts/power_imac19.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-sudo pmset -a proximitywake 0
-sudo pmset -a hibernatemode 3
-sudo pmset -a standbydelaylow 7200
-sudo pmset -a standbydelayhigh 7200
-defaults write com.apple.loginwindow PowerButtonSleepsSystem -bool yes
-pmset -g live
