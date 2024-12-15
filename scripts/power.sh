8a167933ad36ba9d6287c8127e2c99360979f8ee
Elieser <66266691+elieserme@users.noreply.github.com>
2024-12-15 18:46:41 -0300

    Update power.sh

--- scripts/power.sh
+++ scripts/power.sh
@@ -3,6 +3,7 @@ sudo pmset -a proximitywake 0
 sudo pmset -a hibernatemode 0
 sudo pmset -a powernap 0
 sudo pmset -a networkoversleep 0
+sudo pmset -a womp 0
 defaults write com.apple.loginwindow PowerButtonSleepsSystem -bool yes
 # defaults write com.apple.AppleGVA gvaForceAMDAVCDecode -boolean yes
 pmset -g live
